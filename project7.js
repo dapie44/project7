b104e0094ecf75b5ef5543ef0d0b053575f1c4df
Anthony Sandoval <retrovirus546@gmail.com>
2016-12-12 12:14:47 -0800

    started

--- project7.js
+++ project7.js
@@ -12,9 +12,23 @@ function transformPoint(event) {
 
 // Step 2: drawSquare and drawCircle functions
 function drawSquare(x, y, size, color) {
-  // square drawing code here
+  var newRect = document.createElementNS(namespace. "rect")
+  newRect.setAttribute("x", x)
+  newRect.setAttribute("y", y)
+  newRect.setAttribute("width", size)
+  newRect.setAttribute("height", size)
+  newRect.setAttribute("fill", color)
+  screen.appendChild(newRect)
 }
 
+function drawCircle(x, y, r, color) {
+  var newCircle = document.createElementNS(namespace, "circle")
+  newcircle.setAttribute("cx", x)
+  newcircle.setAttribute("cy", y)
+  newcircle.setAttribute("r", r)
+  newcircle.setAttribute("fill", color)
+  screen.appendChild(newCircle);
+}
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
   // what do you want to do when the user presses down
